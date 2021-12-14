a6547540d4e96b571e5b5e9cd1d1eb754a55e77f
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-12-14 23:38:18 +0530

    Create 05_julia-arrays.jl

---
+++ 05_julia-arrays.jl
@@ -0,0 +1,21 @@
+using Printf
+
+
+# 1D Array
+A = [1 2 3 5]
+
+#### Accesing the Elements
+# get first element
+println(first(A))
+# get first n elements
+n = 3
+println(first(A, 3))
+
+# get Last element
+println(last(A))
+# get last n elements
+n = 3
+println(last(A, 3))
+
+
+
