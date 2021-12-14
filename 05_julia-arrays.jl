7005f5415dfd54d24259a06b22db76e4d9c7ddc6
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-12-15 00:31:01 +0530

    Update arrays section

--- 05_julia-arrays.jl
+++ 05_julia-arrays.jl
@@ -2,7 +2,8 @@ using Printf
 
 
 # 1D Array
-A = [1 2 3 5]
+A = [1, 2, 3, 4, 5]
+println(A)
 
 #### Accesing the Elements
 # get first element
@@ -18,4 +19,14 @@ n = 3
 println(last(A, 3))
 
 
+# removing the last element 
+pop!(A)
+println(A)
 
+# removing the first element
+popfirst!(A)
+println(A)
+
+# removing the nth index 
+deleteat!(A, 1)
+println(A)
